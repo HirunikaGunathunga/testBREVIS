9fb22552311b862ec4a8ff432117f04ad4fbe460
Hirunika Gunathunga <84180743+HirunikaGunathunga@users.noreply.github.com>
2021-09-06 10:04:02 +0530

    Add files via upload

---
+++ app/require.php
@@ -0,0 +1,8 @@
+<?php
+    require_once 'core/Controllers.php';
+    require_once 'core/Application.php';
+    require_once 'core/Model.php';
+    require_once 'core/View.php';
+    require_once 'connection/model.php';
+
+    $init = new Application();
